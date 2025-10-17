1289f07b17a4a98cfc1b1b5466cfa2e300a2f3b3
ozgurrozennn <ozen52301@gmail.com>
2025-10-17 23:14:25 +0300

    Update gelir_vergisi.py

--- gelir_vergisi.py
+++ gelir_vergisi.py
@@ -26,7 +26,7 @@ def hesapla_vergi_ve_net(matrah_aylik, gelir_unsuru):
         if yillik_matrah <= 158_000: # Eğer yıllık matrah 158 ve 158 000 altındaysa örnek :  (12_000 x12_000=144_000) yıllık matrah= 144_000*0.15=21_600 yıllık vergi
             vergi = yillik_matrah * 0.15
         elif yillik_matrah <= 330_000:# Eğer yıllık matrah 158.000<yıllık_matrah<=330_000 ise demektir bu kosul . misal (15_000*12)=180_000(180_000-158_00)*0.20 +23_700
-            vergi = 23_700 + 0.20 * (yillik_matrah - 158_000) 
+            vergi = 23_700 + 0.20 * (yillik_matrah - 158_000) +2_400
         elif yillik_matrah <= 1_200_000:
             vergi = 58_100 + 0.27 * (yillik_matrah - 330_000)  
         elif yillik_matrah <= 4_300_000:
@@ -72,6 +72,7 @@ if st.button("Vergiyi Hesapla"):
 
     st.subheader("Sonuc:")#altbaslık metodu ıcın st.subheader
     st.table(df)    # Streamda tablo ile göstermek içindir
+
 
 
 
