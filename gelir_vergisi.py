a651e07e662408722bf082d3cafde3e4438ac165
ozgurrozennn <ozen52301@gmail.com>
2025-10-18 00:01:34 +0300

    Update gelir_vergisi.py

--- gelir_vergisi.py
+++ gelir_vergisi.py
@@ -18,7 +18,7 @@ matrah_aylik = st.number_input(
 
 
 )
- vergi_toplam=2400
+     vergi_toplam=2400
 # def metoduyla oluşturduk, matrah_aylik, gelir_unsuru fonksiyonlarını çağırdık
 def hesapla_vergi_ve_net(matrah_aylik, gelir_unsuru):
     if gelir_unsuru == "Ücretli":
@@ -78,6 +78,7 @@ if st.button("Vergiyi Hesapla"):
 
     st.subheader("Sonuc:")#altbaslık metodu ıcın st.subheader
     st.table(df)    # Streamda tablo ile göstermek içindir
+
 
 
 
